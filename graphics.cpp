9c50a88789560178ecbc6a977705820611b88f5c
Rachel Clark <rclark66@gatech.edu>
2018-05-02 15:59:53 -0400

    remove unnecessary import

--- graphics.cpp
+++ graphics.cpp
@@ -3,9 +3,6 @@
 #include <GLFW/glfw3.h>
 #include <fstream>
 #include <SOIL/SOIL.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include <thread>
 #include "graphics.h"
 
